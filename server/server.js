f5bb3b3d16d32f9cf3933b270fe804180b7bc90b
Peter <pitrens.studio@gmail.com>
2018-12-22 22:04:13 +0800

    Add start script for heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 const path = require('path');
+const express = require('express');
 
 const publicPath =path.join(__dirname, '../public');
+
+const app = express();
+const port = process.env.PORT || 3000 ;
+
+app.use(express.static(publicPath))
+
+app.listen(port , () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
 console.log(publicPath);
