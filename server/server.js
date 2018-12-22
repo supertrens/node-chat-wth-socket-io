8d0c3ebd1a9d93a44ce1e4c9d78674947aa12475
Peter <pitrens.studio@gmail.com>
2018-12-22 22:51:32 +0800

    playground: play with socketIO on client side

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const publicPath =path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');
 
 const app = express();
-const port = process.env.PORT || 3000 ;
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static(publicPath))
+io.on('connection', (socket) => {
+  console.log('New user Connected');
 
-app.listen(port , () => {
+  socket.on('disconnect' , () => {
+    console.log('One user got disconnected');
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
-console.log(publicPath);
